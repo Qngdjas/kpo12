5ba909f9008450f5b64d40fdc6a415a92aa705a4
Qngdjas <qngdjas@mail.ru>
2020-09-10 16:08:03 +0800

    version 1.0

---
+++ src/kpo12/KPO12.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kpo12;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Qngdjas
+ */
+public class KPO12 {
+
+    public static void main(String[] args) {
+        String inputStr = inputString();
+        System.out.println(numSum(inputStr));
+    }
+
+    /**
+     * Функция ввода данных с консоли
+     *
+     * @return Возвращает строку
+     */
+    public static String inputString() {
+        Scanner sc = new Scanner(System.in);
+        //Считываем информацию с консоли
+        String str = sc.nextLine();
+        //Возвращаем результат
+        return str;
+    }
+
+    public static int numSum(String str) {
+        String[] strMass = str.split(" ");
+        int sum = 0;
+        for (int i = 0; i < strMass.length; i++) {
+            sum += Integer.parseInt(strMass[i]);
+        }
+        return sum;
+    }
+}
